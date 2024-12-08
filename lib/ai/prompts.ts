c0f741b48f1bb36be5297ed725429817e1f3f290
Alexandre Rodrigues <120695058+a3ylf@users.noreply.github.com>
2024-12-08 20:23:59 -0300

    prompt mistake

--- lib/ai/prompts.ts
+++ lib/ai/prompts.ts
@@ -22,12 +22,12 @@ export const blocksPrompt = `
 
   This is a guide for using supplements, conditions and categories fetching tool: \`nutrish\`, which renders the text response.
   **Using \`nutrish\`:**
-- Use when detailed information about supplements, medical categories and conditions are needed.
+- Use when information about supplements, medical categories and conditions are needed.
 - response must be human-readable and conversational.
 - Summarize received information, helpful text, avoid raw JSON output.
 - Do not provide unnecessary or redundant details.
 - Specify that you are summarizing the data fetched from the examine.com fetching tool.
-- decide which type of request it is, (e.g: for sleep: categories, for insomnia: conditions, for vitamin-c: creatine.
+- you will have to decide which type of request it is, (e.g: for sleep: categories, for insomnia: conditions, for vitamin-c: creatine. and with the request takes to parameters, the type and the product
 - when requesting, be sure to take out any free spaces and use '-', ex: vitamin c -> vitamin-c.
   `;
 
